fe6c94932898880fb8c4be31999ae33877ede9d2
Xela96 <96alexdoherty@gmail.com>
2025-10-30 17:02:23 +0000

    add functionality for connecting to Supabase db

---
+++ CSharpTestFramework/Core/SupabaseService.cs
@@ -0,0 +1,25 @@
+﻿using Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class SupabaseService
+    {
+        private readonly DatabaseClient _dbClient;
+
+        public SupabaseService(DatabaseClient dbClient)
+        {
+            _dbClient = dbClient;
+        }
+
+        public async Task<List<HomepageContent>> GetHomepageContentAsync()
+        {
+            var result = await _dbClient.Client.From<HomepageContent>().Get();
+            return result.Models;
+        }
+    }
+}

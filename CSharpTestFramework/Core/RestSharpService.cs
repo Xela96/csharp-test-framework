7f963f6ab78b35f1810c77b721a5b5d722de8777
Xela96 <96alexdoherty@gmail.com>
2025-11-04 13:54:08 +0000

    Add RestSharp structure to send GET requests and return response

---
+++ CSharpTestFramework/Core/RestSharpService.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+
+namespace Core
+{
+    public class RestSharpService
+    {
+        private readonly RestClient _siteClient;
+
+        public RestSharpService(RestClient siteClient)
+        {
+            _siteClient = siteClient;
+        }
+
+        public async Task<RestResponse> GetContent(string path="")
+        {
+            var response = await _siteClient.ExecuteAsync(new RestRequest($"/{path}", Method.Get));
+            return response;
+        }
+    }
+}
